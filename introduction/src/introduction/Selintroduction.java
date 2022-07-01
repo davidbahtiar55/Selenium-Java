0e8861023e3fd89d806ec739331d784e6d2c1146
david bahtiar <52740811+davidbahtiar55@users.noreply.github.com>
2022-07-01 07:00:22 +0700

    Update

--- introduction/src/introduction/Selintroduction.java
+++ introduction/src/introduction/Selintroduction.java
@@ -1,8 +1,16 @@
 package introduction;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Selintroduction {
 
 	public static void main(String [] args) {
-		
+		System.setProperty("wWbDriver.Chrome.driver", "C:\\chromedriver_win32");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");		
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
 	}
 }
