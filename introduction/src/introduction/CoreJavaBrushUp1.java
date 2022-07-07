ccd492dbe26c79f027724e89b5b5a11565ad798d
david bahtiar <davidbahtiar55@gmail.com>
2022-07-07 20:27:43 +0700

    Recommit

--- introduction/src/introduction/CoreJavaBrushUp1.java
+++ introduction/src/introduction/CoreJavaBrushUp1.java
@@ -12,7 +12,38 @@ public class CoreJavaBrushUp1 {
 		boolean myCard = true;
 		
 		System.out.println(myNum+"is the value stored in the myNum variable");
-		System.out.print(website);
+		System.out.println(website);
+		//Arrays -
+		int [] arr = new int [5];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 4;
+		arr[3] = 5;
+		arr[4] = 6;
+		
+		int[] arr2 = {1,2,4,5,6,7,8,9,9,10,122};
+		
+//		//for loop arr.length - 5
+//		for(int i = 1; i< arr.length; i++) //2
+//		{
+//			System.out.println(arr[i]);
+//		}
+			for (int i =0; i<arr2.length; i++)
+			{
+				System.out.println(arr2[i]);
+			}
+			
+			String[] name = {"rahul", "shetty", "selenium"};
+			for(int i =0; i<name.length;i++)
+			{
+				System.out.println(name [i]);
+			}
+			
+			for ( String s: name)
+			{
+				System.out.println(s);
+			}
+		}
 	}
 
-}
+
--- introduction/src/introduction/Selintroduction.java
+++ introduction/src/introduction/Selintroduction.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Selintroduction {
 
 	public static void main(String [] args) {
-		System.setProperty("wWbDriver.Chrome.driver", "C:\\chromedriver_win32");
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com");		
 		System.out.println(driver.getTitle());
