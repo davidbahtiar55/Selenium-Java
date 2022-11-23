af258efcd37265588b9a271b43737a8ec6110464
david bahtiar <52740811+davidbahtiar55@users.noreply.github.com>
2022-11-23 09:16:48 +0700

    Update Code
    (+) Add file Locators
    (=) Fixing code Selintroduction

--- introduction/src/introduction/Locators.java
+++ introduction/src/introduction/Locators.java
@@ -1,5 +1,18 @@
 package introduction;
 
-public class Locators {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class Locators {
+	
+	public static void main(String [] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.id("inputUsername")).sendKeys("david");
+		driver.findElement(By.name("inputPassword")).sendKeys("admin123");
+		driver.findElement(By.className("signInBtn")).click();
+
+}
 }
--- introduction/src/introduction/Selintroduction.java
+++ introduction/src/introduction/Selintroduction.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Selintroduction {
 
 	public static void main(String [] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com");		
 		System.out.println(driver.getTitle());
