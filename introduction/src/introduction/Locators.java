59062b93ea41badb7e67c9e7e53d084ff73088f6
david bahtiar <52740811+davidbahtiar55@users.noreply.github.com>
2022-11-29 09:59:09 +0700

    Update Pom &  Locators
    (+) Add Config Pom Xml
    (+) Add Coding Locators

--- introduction/src/introduction/Locators.java
+++ introduction/src/introduction/Locators.java
@@ -1,5 +1,7 @@
 package introduction;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,11 +11,16 @@ public class Locators {
 	public static void main(String [] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("david");
 		driver.findElement(By.name("inputPassword")).sendKeys("admin123");
 		driver.findElement(By.className("signInBtn")).click();
-
-
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Bahtiar");
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("tiar@rsa.com");
+		driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("bahtiar@gmail.com");
 }
 }
